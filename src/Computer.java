802f51348cb5f289469a2f99fc8dad50d15388a3
Elnur Absamatov <elnurabsamatov2020@gmail.com>
2021-03-22 20:07:16 +0500

    commit message

--- src/Computer.java
+++ src/Computer.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Computer {
+public class Computer {
+    int ssd;
+    int ram;
+
+    Computer(){
+        System.out.println("Я создался");
+    }
+    Computer(int ssd){
+        this.ssd = ssd;
+        System.out.println("Ssd: " + ssd);
+        System.out.println("Ram: " + ram);
+    }
+    Computer(int ssd, int ram){
+        this.ssd = ssd;
+        this.ram = ram;
+        System.out.println("Ssd: " + ssd);
+        System.out.println("Ram: " + ram);
+    }
 }
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Computer computer = new Computer();
+        Computer computer2 = new Computer(120);
+        Computer computer3 = new Computer(120, 6);
+
+    }
+}
